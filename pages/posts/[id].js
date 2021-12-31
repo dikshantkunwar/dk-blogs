fcb3498a94a8e720179d8fb8e6354be35c5c1c38
Dikshant Kunwar <dikshantkunwar@gmail.com>
2021-12-31 02:00:19 -0500

    generated pages with dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,14 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
 export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
@@ -8,6 +16,14 @@ export async function getStaticPaths() {
     fallback: false
   }
 }
-export default function Post() {
-  return <Layout>...</Layout>
+export default function Post({ postData }) {
+  return (
+  <Layout>
+    {postData.title}
+    <br />
+    {postData.id}
+    <br />
+    {postData.date}
+  </Layout>
+  )
 }
