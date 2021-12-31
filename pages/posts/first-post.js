21b03ad395977d59ec0dc5484681928ad6766465
Dikshant Kunwar <dikshantkunwar@gmail.com>
2021-12-30 21:34:29 -0500

    include profile image, and added nextjs script tag in first post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,26 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <Script
+      src="https://connect.facebook.net/en_US/sdk.js"
+      strategy='lazyOnload'
+      onLoad={() =>
+        console.log('script loaded sucessfully, window.FB populated')
+      }
+    />
+    <h1>First Post</h1>
+    <h2>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </h2>
     </>
   )
 }
