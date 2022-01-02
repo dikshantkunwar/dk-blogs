58929c96704b17e02d7dc979ee69a74b06daa8c1
Dikshant Kunwar <dikshantkunwar@gmail.com>
2022-01-01 21:43:26 -0500

    added date component, fixed the async issue and updated id page with head title

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,9 @@
 import Layout from '../../components/layout'
+import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -19,11 +20,15 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
   <Layout>
-    {postData.title}
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
     <br />
     {postData.id}
     <br />
     {postData.date}
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>
   )
 }
