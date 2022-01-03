38d229c834f23389ef60d716bd3e3136229cd4f7
Dikshant Kunwar <dikshantkunwar@gmail.com>
2022-01-02 20:41:19 -0500

    added nav js and css

---
+++ components/nav.js
@@ -0,0 +1,11 @@
+import Styles from '../nav.module.css'
+
+export default function Nav() {
+  return (
+    <div className='nav'>
+    <span>tweeter</span>
+    <div class="newtweet">
+    </div>
+    </div>
+  )
+}
